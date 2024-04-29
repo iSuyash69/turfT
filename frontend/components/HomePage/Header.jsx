7b464a7333dd8d0ed6b9f396766badf8ac67fceb
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2024-04-29 16:56:57 +0530

    Update Header.jsx

--- frontend/components/HomePage/Header.jsx
+++ frontend/components/HomePage/Header.jsx
@@ -7,7 +7,7 @@ const Header=()=>{
         <div className="flex w-full pt-10 px-20"> 
             <div className="relative overflow-hidden rounded-3xl w-full h-80 ">
                 <div className="absolute flex flex-col gap-2 items-center justify-center z-10 top-0 left-0 w-full h-full bg-gray-400 bg-opacity-20 backdrop-blur-[2px]">
-                    <p className="text-[32px] text-gray-800 font-bold">Book Your turf slot with TurfTime</p>
+                    <p className="text-[32px] text-gray-800 font-bold">Book Your turf slot with FieldFiesta</p>
                     <p className="font-semibold text-lg text-gray-800">20+ turfs available for booking worldwide!</p>
                 </div>
                 <Image className="w-full h-full object-fill" src={turfImg}></Image>
