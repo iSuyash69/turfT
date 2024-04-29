4f81b048ecdffb95b35ef1a244efc45d4a457b17
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2024-04-29 16:56:04 +0530

    Update NavBar.jsx

--- frontend/components/NavBar/NavBar.jsx
+++ frontend/components/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar=()=>{
     return(
         <div>
             <div className="fixed z-20 top-0 flex w-full items-center bg-opacity-90 backdrop-blur-sm justify-between bg-blue-100 py-5 px-10">
-                <div onClick={()=>{router.push("/")}} className="text-xl font-[600] cursor-pointer">TurfTime</div>
+                <div onClick={()=>{router.push("/")}} className="text-xl font-[600] cursor-pointer">FieldFiesta</div>
                 <div className="flex gap-5 list-none items-center">
                 <button onClick={()=>setMyBookingsPopUp(true)} type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-4 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 ">My Bookings</button>
                 <li className="cursor-pointer font-medium">About Us</li>
